7fff1440a3454507bee4dc57c49fe0196ab52bde
amar-295 <612amar@gmail.com>
2026-03-29 16:31:41 +0530

    feat: initialize TypeScript project with basic setup and configuration

--- basics.ts
+++
@@ -1,5 +0,0 @@
-function greet (name: string): string {
-    return `hello ${name}`
-}
-
-console.log(greet("Amarnath"));
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function greet(person : string): string {
+    return `Hello ${person}`
+}
+
+const username: string = "Amarnath"
+console.log(greet(username));
