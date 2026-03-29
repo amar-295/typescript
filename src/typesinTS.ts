facf0a32d240a96881f2f2222bf0e1ac6e730fdb
amar-295 <612amar@gmail.com>
2026-03-29 16:47:24 +0530

    feat: add initial TypeScript code for annotation and inference example

---
+++ src/typesinTS.ts
@@ -0,0 +1,11 @@
+// annotation and inference
+
+let drink = "lemonade"
+
+let mug = Math.random() > 0.5 ? 10 : 5
+
+let courseName = "BCA"
+
+let subject: string = "Computer Graphics"
+subject = "Computer System Architecture"
+
